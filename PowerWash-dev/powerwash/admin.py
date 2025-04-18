b1d326fd474b27082b3cb088986b84c0e46004ff
Bruno Oliveira <142051333+ManoBG02@users.noreply.github.com>
2025-04-18 01:35:37 -0400

    Delete PowerWash-dev directory

--- PowerWash-dev/powerwash/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ServicePage, Service, Profile
-
-
-#Register Your Models Here, Sir/Ma'am.
-
-admin.site.register(ServicePage)
-admin.site.register(Service)
-admin.site.register(Profile)
